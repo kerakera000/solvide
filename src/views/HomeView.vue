3950fa93a831cae156324f3eaecdad8936fe3c0f
kerakera000 <leon51414@icloud.com>
2023-03-10 18:10:19 +0900

    セーブ

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,393 @@
 <template>
-  <div class="home">
-    <div class="text">test</div>
-  </div>
+  <section class="kv">
+    <div class="kv__text-box">
+        <h2 class="kv__text-box--title">結果につながるサイトには<br>必ず理由がある</h2>
+        <p class="kv__text-box--subtitle">Solvideはデザインからサイト構築、保守運用まで一括で担当しているホームページ制作グループです。</p>
+        <img class="kv__text-box--img-title-back" src="../assets/images/kv-img/kv-title-back.png" alt="">
+        <img class="kv__text-box--img-text-back" src="../assets/images/kv-img/kv-text-back.png" alt="">
+
+    </div>
+    <div class="kv__img-box">
+        <img class="kv__img-box--img" src="../assets/images/kv-img/kv.png" alt="">
+        <img class="kv__img-box--img-back" src="../assets/images/kv-img/kv-back.png" alt="">
+    </div>
+  </section>
+
+  <section class="about">
+      <div class="about__img-box">
+          <div class="about__img-box--light">
+              <img class="about__img-box--light--img" src="../assets/images/about-img/about-light.png" alt="">
+          </div>
+          <div class="about__img-box--light-back">
+              <img class="about__img-box--light-back--img" src="../assets/images/about-img/about-light-back.png" alt="">
+          </div>
+      </div>
+      <div class="about__text-box">
+          <h2 class="about__text-box--title">「わかりやすさ」と「デザイン」で<br> お客様目線に立ったものをつくる</h2>
+          <p class="about__text-box--text">デザインはアートではなく、問題を解決するためのものです。 私たちが無意識に見ている広告や商品も製作者の”意図”があります。 例えば、「こうしたら使いやすい」「こういう印象を与えたい」など。 会社やサービスの顔となるホームページにもお客様に伝えるための 意図を散りばめて見た目だけじゃないサイトをつくりあげます。</p>
+          <button class="about__text-box--button">
+              ご依頼はこちら
+              <img class="about__text-box--button--img" src="" alt="">
+          </button>
+      </div>
+  </section>
+
+  <section class="service">
+      <div class="service__title">
+          <div class="title">
+              <h2 class="title__main-text">事業内容</h2>
+              <div class="title__line"></div>
+              <p class="title__sub-text">SERVICE</p>
+          </div>
+      </div>
+      <div class="service__web">
+          <div class="service__web--title">
+              <h3 class="service__web--title--text">WEB制作</h3>
+          </div>
+          <div class="service__web--content">
+              <div class="web">
+                  <div class="web__title">
+                      <div class="web__title--cnt">
+                          <h4 class="web__title--cnt--text">Service</h4>
+                          <span class="web__title--cnt--num">01</span>
+                      </div>
+                      <div class="web__title--line"></div>
+                  </div>
+
+                  <div class="web__body">
+                      <div class="display-content">
+                          <div class="web__body--title">
+                              <h5 class="web__body--title--text">ワイヤーフレーム / デザイン案</h5>
+                          </div>
+                          <div class="web__body--text-box">
+                              <p class="web__body--text-box--text">
+                                  お客様からのヒアリングをもとに(設計図)を作成した後デザインを作成します。デザインはTOP+4ページでスマホ版も作成します。 画像と文章はお客様が用意してくださることが望ましいですが、こちらで用意することも可能です。
+                              </p>
+                          </div>
+                      </div>
+                      <div class="web__body--img-cnt">
+                          <div class="web__body--img-cnt--box">
+                              <img class="web__body--img-cnt--box--img web-img-1" src="../assets/images/service-img/service1.png" alt="">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="web">
+                  <div class="web__title">
+                      <div class="web__title--cnt">
+                          <h4 class="web__title--cnt--text">Service</h4>
+                          <span class="web__title--cnt--num">02</span>
+                      </div>
+                      <div class="web__title--line"></div>
+                  </div>
+
+                  <div class="web__body">
+                      <div class="display-content">
+                          <div class="web__body--title">
+                              <h5 class="web__body--title--text">サイト構築</h5>
+                          </div>
+                          <div class="web__body--text-box">
+                              <p class="web__body--text-box--text">
+                                  作成したデザインを元にサイトを構築して、デザインを元に適切なアニメーションや機能を採用します。 お客様の事業の魅力が伝わりやすく、且つ使いやすいホームぺージを作っていきます。
+                              </p>
+                          </div>
+                      </div>
+                      <div class="web__body--img-cnt">
+                          <div class="web__body--img-cnt--box">
+                              <img class="web__body--img-cnt--box--img web-img-2" src="../assets/images/service-img/service2.png" alt="">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="web">
+                  <div class="web__title">
+                      <div class="web__title--cnt">
+                          <h4 class="web__title--cnt--text">Service</h4>
+                          <span class="web__title--cnt--num">03</span>
+                      </div>
+                      <div class="web__title--line"></div>
+                  </div>
+
+                  <div class="web__body">
+                      <div class="display-content">
+                          <div class="web__body--title">
+                              <h5 class="web__body--title--text">サイトリニューアル</h5>
+                          </div>
+                          <div class="web__body--text-box">
+                              <p class="web__body--text-box--text">
+                              現状のサイトの問題をヒアリングで洗い出し、お客様の求めるゴールを一緒に解決できるよう、ホームページを作っていきます。ページの増設やレイアウトの変更など問題点を分析し、お客様と解決していきます。
+                              </p>
+                          </div>
+                      </div>
+                      <div class="web__body--img-cnt">
+                          <div class="web__body--img-cnt--box">
+                              <img class="web__body--img-cnt--box--img web-img-3" src="../assets/images/service-img/service3.png" alt="">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="web">
+                  <div class="web__title">
+                      <div class="web__title--cnt">
+                          <h4 class="web__title--cnt--text">Service</h4>
+                          <span class="web__title--cnt--num">04</span>
+                      </div>
+                      <div class="web__title--line"></div>
+                  </div>
+
+                  <div class="web__body">
+                      <div class="display-content">
+                          <div class="web__body--title">
+                              <h5 class="web__body--title--text">充実のサポート</h5>
+                          </div>
+                          <div class="web__body--text-box">
+                              <p class="web__body--text-box--text">
+                              サービス購入時から、納品後までサポートが充実しています。サイト構築に必要な文章や画像、サイトを表示する際に必要になるサーバーやドメインの取得や設定WordPressの初期設定もすべて対応可能です。納品後はトラブル対応や、修正などの定期更新も承っております。
+                              </p>
+                          </div>
+                      </div>
+                      <div class="web__body--img-cnt">
+                          <div class="web__body--img-cnt--box">
+                              <img class="web__body--img-cnt--box--img web-img-4" src="../assets/images/service-img/service4.png" alt="">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <section class="flow">
+      <div class="flow__title">
+          <div class="title">
+              <h2 class="title__main-text">制作フロー</h2>
+              <div class="title__line"></div>
+              <p class="title__sub-text">FLOW</p>
+          </div>
+      </div>
+      <div class="flow__cnt">
+          <div class="flow-cnt">
+              <div class="flow-cnt__box">
+                  <div class="flow-cnt__box--img-box">
+                      <div class="flow-cnt__box--img-box--size">
+                          <img class="flow-cnt__box--img-box--size--img" src="../assets/images/flow-img/flow1.png" alt="">
+                      </div>
+                  </div>
+                  <div class="flow-cnt__box--text-box">
+                      <p class="flow-cnt__box--text-box--text">「見積もり・カスタマイズの相談」からお問い合わせください</p>
+                  </div>
+              </div>
+              <div class="flow-cnt__box">
+                  <div class="flow-cnt__box--img-box">
+                      <div class="flow-cnt__box--img-box--size">
+                          <img class="flow-cnt__box--img-box--size--img" src="../assets/images/flow-img/flow2.png" alt="">
+                      </div>
+                  </div>
+                  <div class="flow-cnt__box--text-box">
+                      <p class="flow-cnt__box--text-box--text">作りたい内容をヒアリングし、お見積もりをご確認いただきます</p>
+                  </div>
+              </div>
+              <div class="flow-cnt__box">
+                  <div class="flow-cnt__box--img-box">
+                      <div class="flow-cnt__box--img-box--size">
+                          <img class="flow-cnt__box--img-box--size--img" src="../assets/images/flow-img/flow3.png" alt="">
+                      </div>
+                  </div>
+                  <div class="flow-cnt__box--text-box">
+                      <p class="flow-cnt__box--text-box--text">文章・画像などのご準備いただく物の有無の確認をします。</p>
+                  </div>
+              </div>
+          </div>
+
+          <div class="flow__cnt--line"></div>
+
+          <div class="flow-cnt">
+              <div class="flow-cnt__box">
+                  <div class="flow-cnt__box--img-box">
+                      <div class="flow-cnt__box--img-box--size">
+                          <img class="flow-cnt__box--img-box--size--img" src="../assets/images/flow-img/flow4.png" alt="">
+                      </div>
+                  </div>
+                  <div class="flow-cnt__box--text-box">
+                      <p class="flow-cnt__box--text-box--text">制作スタート<br>デザイン提案 ->修正 -> サイト構築->微調整</p>
+                  </div>
+              </div>
+              <div class="flow-cnt__box">
+                  <div class="flow-cnt__box--img-box">
+                      <div class="flow-cnt__box--img-box--size">
+                          <img class="flow-cnt__box--img-box--size--img" src="../assets/images/flow-img/flow5.png" alt="">
+                      </div>
+                  </div>
+                  <div class="flow-cnt__box--text-box">
+                      <p class="flow-cnt__box--text-box--text">ズレや不十分な点がないかの最終チェックをします</p>
+                  </div>
+              </div>
+              <div class="flow-cnt__box">
+                  <div class="flow-cnt__box--img-box">
+                      <div class="flow-cnt__box--img-box--size">
+                          <img class="flow-cnt__box--img-box--size--img" src="../assets/images/flow-img/flow6.png" alt="">
+                      </div>
+                  </div>
+                  <div class="flow-cnt__box--text-box">
+                      <p class="flow-cnt__box--text-box--text">納品形式はZipファイルもしくはWordpressに直接 アップロードします。</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <section class="works">
+      <div class="works__title">
+          <div class="title">
+              <h2 class="title__main-text">制作実績</h2>
+              <div class="title__line"></div>
+              <p class="title__sub-text">WORKS</p>
+          </div>
+      </div>
+      <div class="works__swiper">
+        <swiper
+          :slidesPerView="3"
+          :spaceBetween="10"
+          :loop="true"
+          :pagination="{
+            clickable: true,
+          }"
+          :navigation="true"
+          :modules="modules"
+          class="mySwiper"
+        >
+          <swiper-slide class="red">Slide 1</swiper-slide>
+          <swiper-slide class="red">Slide 2</swiper-slide><swiper-slide class="red">Slide 3</swiper-slide>
+          <swiper-slide class="red">Slide 4</swiper-slide><swiper-slide class="red">Slide 5</swiper-slide>
+          <swiper-slide class="red">Slide 6</swiper-slide><swiper-slide class="red">Slide 7</swiper-slide>
+          <swiper-slide class="red">Slide 8</swiper-slide><swiper-slide class="red">Slide 9</swiper-slide>
+        </swiper>
+      </div>
+  </section>
+
+  <section class="profile">
+      <div class="profile__title">
+          <div class="title">
+              <h2 class="title__main-text">プロフィール</h2>
+              <div class="title__line"></div>
+              <p class="title__sub-text">Profile</p>
+          </div>
+      </div>
+      <div class="profile__cnt">
+          <div class="profile__cnt--img-box-left">
+              <img class="profile__cnt--img-box-left--img" src="../assets/images/profile-img/profile-left.png" alt="">
+          </div>
+          <div class="profile__cnt--img-box-right">
+              <img class="profile__cnt--img-box-right--img" src="../assets/images/profile-img/profile-right.png" alt="">
+          </div>
+          <div class="profile__cnt--box">
+              <div class="profile-row">
+                  <div class="profile-row--title">
+                      <h3 class="profile-row--title--text">グループ名</h3>
+                  </div>
+                  <div class="profile-row--text-box">
+                      <p class="profile-row--text-box--text">Solvide制作グループ</p>
+                  </div>
+              </div>
+              <div class="profile__cnt--box--text"></div>
+              <div class="profile-row">
+                  <div class="profile-row--title">
+                      <h3 class="profile-row--title--text">設立</h3>
+                  </div>
+                  <div class="profile-row--text-box">
+                      <p class="profile-row--text-box--text">2022年8月1日</p>
+                  </div>
+              </div>
+              <div class="profile__cnt--box--text"></div>
+              <div class="profile-row">
+                  <div class="profile-row--title">
+                      <h3 class="profile-row--title--text">代表</h3>
+                  </div>
+                  <div class="profile-row--text-box">
+                      <p class="profile-row--text-box--text">吉澤 紫魔</p>
+                  </div>
+              </div>
+              <div class="profile__cnt--box--text"></div>
+              <div class="profile-row">
+                  <div class="profile-row--title">
+                      <h3 class="profile-row--title--text">従業員</h3>
+                  </div>
+                  <div class="profile-row--text-box">
+                      <p class="profile-row--text-box--text">2人</p>
+                  </div>
+              </div>
+              <div class="profile__cnt--box--text"></div>
+              <div class="profile-row">
+                  <div class="profile-row--title">
+                      <h3 class="profile-row--title--text">お問い合わせ</h3>
+                  </div>
+                  <div class="profile-row--text-box">
+                      <p class="profile-row--text-box--text">Solvideココナラ</p>
+                  </div>
+              </div>
+              <div class="profile__cnt--box--text"></div>
+          </div>
+      </div>
+  </section>
+
+  <section class="contact">
+      <div class="contact__cnt">
+          <div class="contact__cnt--img-box">
+              <div class="contact__cnt--img-box--item-right">
+                  <img class="contact__cnt--img-box--item-right--img" src="../assets/images/contact-img/contact-right.png" alt="">
+              </div>
+              <div class="contact__cnt--img-box--item-left">
+                  <img class="contact__cnt--img-box--item-left--img" src="../assets/images/contact-img/contact-left.png" alt="">
+              </div>
+              <div class="contact__cnt--img-box--size">
+                  <img class="contact__cnt--img-box--size--img" src="../assets/images/contact-img/contact-main.png" alt="">
+              </div>
+          </div>
+          <div class="display-content">
+              <div class="contact-title-box">
+                  <h2 class="contact-title-box__text">魅力が伝わるには理由がある。そんなサイトをあなたに。</h2>
+              </div>
+              <div class="contact-cnt">
+                  <div class="contact-cnt__text-box">
+                      <p class="contact-cnt__text-box--text">ご依頼、ご相談の方はココナラにて受け付けております。ご不明点等ございましたらお問い合わせください。</p>
+                  </div>
+                  <div class="contact-cnt__button-cnt">
+                      <div class="contact-cnt__button-cnt--box">
+                          <p class="contact-cnt__button-cnt--box--text">ココナラで依頼</p>
+                          <img class="contact-cnt__button-cnt--box--img" src="" alt="">
+                      </div>
+                  </div>
+              </div>
+              <div class="contact-item">
+                  <img class="contact-item__img" src="../assets/images/profile-img/profile-right.png" alt="">
+              </div>
+          </div>
+      </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-export default {
-}
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  import 'swiper/css/pagination';
+  import 'swiper/css/navigation';
+
+  // import required modules
+  import { Pagination, Navigation } from 'swiper';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Pagination, Navigation],
+      };
+    },
+  };
 </script>
