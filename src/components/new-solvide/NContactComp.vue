d722517b287d90c2fea9190356506687e2ab4a4e
kerakera000 <leon51414@icloud.com>
2023-04-05 03:59:29 +0900

    セーブ

---
+++ src/components/new-solvide/NContactComp.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="NContact">
+        <div>
+
+        </div>
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+        <div>
+            <div class="main-contact-btn">
+                <button>VIEW MORE</button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'NContactComp',
+    }
+</script>
+
+<style lang="scss">
+
+</style>
--- src/views/NewSolvideView.vue
+++ src/views/NewSolvideView.vue
@@ -1,14 +1,29 @@
 <template>
     <NKVComp/>
+    <NAbout/>
+    <NAService/>
+    <NWorkComp/>
+    <NBlogsComp/>
+    <NContactComp/>
 </template>
 
 <script>
     import NKVComp from '@/components/new-solvide/NKVComp.vue';
+    import NAbout from '@/components/new-solvide/NAboutComp.vue';
+    import NAService from '@/components/new-solvide/NServiceComp.vue';
+    import NWorkComp from '@/components/new-solvide/NWorkComp.vue';
+    import NBlogsComp from '@/components/new-solvide/NBlogsComp.vue';
+    import NContactComp from '@/components/new-solvide/NContactComp.vue';
 
     export default {
         name: 'HomeView',
         components: {
             NKVComp,
+            NAbout,
+            NAService,
+            NWorkComp,
+            NBlogsComp,
+            NContactComp,
         },
     };
 </script>
